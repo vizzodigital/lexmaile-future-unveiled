2172e42ac31b1537e73f70ba4b78fea6c3332a00
Romy Furtado <rofrtd@gmail.com>
2025-06-12 13:49:47 +1000

    [FEAT] GH pages setup

--- vite.config.ts
+++ vite.config.ts
@@ -5,15 +5,14 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/lexmaile-future-unveiled/", // <-- Adicione esta linha
   server: {
     host: "::",
     port: 8080,
   },
-  plugins: [
-    react(),
-    mode === 'development' &&
-    componentTagger(),
-  ].filter(Boolean),
+  plugins: [react(), mode === "development" && componentTagger()].filter(
+    Boolean
+  ),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
